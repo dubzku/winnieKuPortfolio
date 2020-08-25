a025934ba6578e9ee1eb218d05572cc199d450b3
Winnie Ku <winn.ku@gmail.com>
2020-08-25 17:48:21 -0400

    :pray: added thank you page for form submission, error handling, and scroll effects

--- scripts/script.js
+++ scripts/script.js
@@ -8,11 +8,13 @@ $(document).ready(function(){
 
 // Init
 portfolio.init = () => {
-    console.log('init is connected');
     portfolio.introHeading();
+    portfolio.scrollEffects();
+    portfolio.submitForm();
+    console.log('init is working!');
 }
 
-// intro heading
+// Typed.js function in header intro 
 portfolio.introHeading = () => {
     let options = {
         strings: ["", "Front-End Developer", "Avid Proofreader", "Emoji Enthusiast", "Toronto-based", "Let's work together"],
@@ -22,4 +24,25 @@ portfolio.introHeading = () => {
     };
 
     let typed = new Typed('.introduction', options);
+}
+
+// Scrolling effects when links in nav bar are clicked
+portfolio.scrollEffects = () => {
+    $('.scroll').click(function () {
+        $('html,body').animate({ scrollTop: $(this.hash).offset().top }, 575);
+    });
+} 
+
+// Error handling for if user hits "Submit" without typing anything in one of the fields
+portfolio.submitForm = () => {
+    $('form').on('submit', (event) => {
+        const userName = $("input[type=text]").val();
+        const userEmail = $("input[type=email]").val();
+        const userMessage = $("textarea").val();
+
+        if (!userName || !userEmail || !userMessage) {
+            event.preventDefault();
+            swal("Oops!", "Please complete all fields!");
+        }
+    })
 }
